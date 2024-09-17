1867160449ec38cd50cfba66002160b6d18cbc2a
SanFreige <santiago.freige@gmail.com>
2024-09-17 11:38:21 -0300

    Agrego estructura base, entidades y controller de usuario

---
+++ Servidor/Infrastructure/Tienda.cs
@@ -0,0 +1,13 @@
+﻿namespace Servidor.Infrastructure
+{
+    public class Tienda
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public string Provincia { get; set; }
+        public bool Deshabilitado { get; set; }
+
+    }
+}
---
+++ Servidor/Infrastructure/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace Servidor.Infrastructure
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreDeUsuario { get; set; }
+        public string Password { get; set; }
+        public Tienda Tienda { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public bool Deshabilitado { get; set; }
+    }
+}
